82fcdc84c46b3cdc98815a8c73b53b9c01f58b8e
Artem Taran <artt.zima@protonmail.com>
2023-11-04 19:07:37 +0200

    Add tests

--- src/compare.js
+++ src/compare.js
@@ -7,31 +7,31 @@ const compare = (first, second) => {
   const all = _.union(firstKeys, secondKeys).sort();
   const common = _.intersection(firstKeys, secondKeys);
   const onlyInFirst = _.difference(firstKeys, secondKeys);
-  const onlyInSecond = _.difference(firstKeys, secondKeys);
+  const onlyInSecond = _.difference(secondKeys, firstKeys);
 
   const diff = all.flatMap((key) => {
+    let item;
     if (onlyInFirst.includes(key)) {
       const value = first[key];
-      return ` - ${key}: ${value}`;
-    }
-
-    if (onlyInSecond.includes(key)) {
+      item = ` - ${key}: ${value}`;
+    } else if (onlyInSecond.includes(key)) {
       const value = second[key];
-      return ` + ${key}: ${value}`;
-    }
-
-    if (common.includes(key)) {
+      item = ` + ${key}: ${value}`;
+    } else if (common.includes(key)) {
       const value1 = first[key];
       const value2 = second[key];
       if (value1 === value2) {
-        return `   ${key}: ${value1}`;
+        item = `   ${key}: ${value1}`;
+      } else {
+        item = [` - ${key}: ${value1}`, ` + ${key}: ${value2}`];
       }
-      return [` - ${key}: ${value1}`, ` + ${key}: ${value2}`];
     }
+
+    return item;
   });
 
   const result = `{\n${diff.join('\n')}\n}`;
-  console.log(result);
+  return result;
 };
 
 export default compare;
